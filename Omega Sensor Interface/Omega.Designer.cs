6f4a0bbec4d7f3afdb6805167b84adac0b4d5662
manuel11g <manuel.galiu@gmail.com>
2017-06-01 21:23:13 +0200

    Initial check-in

---
+++ Omega Sensor Interface/Omega.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Omega_Sensor_Interface
+{
+    partial class Omega
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Status = new System.Windows.Forms.TabPage();
+            this.Config = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControl1.Controls.Add(this.Status);
+            this.tabControl1.Controls.Add(this.Config);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 60);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(745, 463);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Status
+            // 
+            this.Status.Location = new System.Drawing.Point(28, 4);
+            this.Status.Name = "Status";
+            this.Status.Padding = new System.Windows.Forms.Padding(3);
+            this.Status.Size = new System.Drawing.Size(713, 455);
+            this.Status.TabIndex = 0;
+            this.Status.Text = "Status";
+            this.Status.UseVisualStyleBackColor = true;
+            // 
+            // Config
+            // 
+            this.Config.Location = new System.Drawing.Point(28, 4);
+            this.Config.Name = "Config";
+            this.Config.Padding = new System.Windows.Forms.Padding(3);
+            this.Config.Size = new System.Drawing.Size(713, 455);
+            this.Config.TabIndex = 1;
+            this.Config.Text = "Config";
+            this.Config.UseVisualStyleBackColor = true;
+            // 
+            // Omega
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Omega";
+            this.Text = "Omega";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Status;
+        private System.Windows.Forms.TabPage Config;
+    }
+}
